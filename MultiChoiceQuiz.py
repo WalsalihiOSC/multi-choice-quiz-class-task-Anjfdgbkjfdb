0c605ecba9e33864f27acc60222eb165167125e4
bobo <79424554+Anjfdgbkjfdb@users.noreply.github.com>
2022-04-01 10:01:17 +1300

    Buttons
    Added in radio buttons and questions

--- MultiChoiceQuiz.py
+++ MultiChoiceQuiz.py
@@ -1,2 +1,23 @@
 #week 9 multi choice task
 from tkinter import*
+
+class Multi:
+    def __init__(self, parent):
+
+        Label(parent,text="Q1. What colour does yellow and red make?").grid()
+
+        self.select=StringVar()
+        self.select.set(0)
+        ans = ['Purple', 'Blue', 'Orange','Green']
+        for item in ans:
+            Radiobutton(text=item,value=item,variable=self.select).grid(padx=10)
+        
+        
+
+        
+
+
+if __name__ == "__main__":        
+    root = Tk()
+    Multi(root)
+    root.mainloop()
