f1d89ebb8a62bcffa72d537dd81aafd792950f47
bobo <79424554+Anjfdgbkjfdb@users.noreply.github.com>
2022-04-05 10:58:10 +1200

    Positioned buttons, question, and answer

--- MultiChoiceQuiz.py
+++ MultiChoiceQuiz.py
@@ -4,27 +4,25 @@ from tkinter import*
 class Multi:
     def __init__(self, parent):
 
-        Label(parent,text="Q1. What colour does yellow and red make?").grid()
-
+        Label(parent,text="Q1. What colour does yellow and red make?").grid(row=1,column=1,columnspan=3)
         self.select=StringVar()
         self.select.set(0)
         ans = ['Purple', 'Blue', 'Orange','Green']
         for item in ans:
-            userinput=Radiobutton(text=item,value=item,variable=self.select, command=self.check)
-            userinput.grid()
+            userinput=Radiobutton(text=item,value=item,variable=self.select, width=25,command=self.check)
+            userinput.grid(column=2)
         self.text=Label(parent,text="")
-        self.text.grid()
+        self.text.grid(row=6,column=2)
 
     def check(self):
         if self.select.get() == "Orange":
-            self.text.configure(text="correct")
+            self.text.configure(text="Correct")
             self.text.grid()
         else:
-            self.text.configure(text="incorrect")
+            self.text.configure(text="Incorrect")
             self.text.grid()
 
 
-if __name__ == "__main__":        
-    root = Tk()
-    Multi(root)
-    root.mainloop()
+root = Tk()
+Multi(root)
+root.mainloop()
